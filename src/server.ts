0f3fc965aa09fa8e246e973590ea7593d42447fe
João Nogueira <joaoguinogueira04@gmail.com>
2023-12-27 02:31:02 -0300

    métodos criados

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import fastfy from 'fastify';
+import { Dirent } from 'fs';
 import { z } from 'zod';
 
 const app = fastfy();
@@ -12,28 +13,118 @@ app.get('/drivers', async () => {
     return { drivers };
 })
 
-app.post('/drivers', async (request, reply) => {
+app.post('/drivers/create', async (request, reply) => {
     const createDriverSchema = z.object({
         name: z.string(),
-        phone_number: z.string()
+        phone_number: z.string(),
+        man: z.boolean()
     });
-    const {name, phone_number} = createDriverSchema.parse(request.body);
+    const { name, phone_number, man } = createDriverSchema.parse(request.body);
 
     await prisma.driver.create({
         data: {
-            name, 
-            phone_number
+            name,
+            phone_number,
+            man
         }
     });
 
     return reply
         .code(201)
         .header("Content-type", "application/json;charset=utf-8")
-        .send({
-        "message": "success"});
+        .send({ "message": "success" });
+})
+
+app.post('/drivers/update', async (request, reply) => {
+    const appJson = z.object(
+        {
+            appPackageName: z.string(),
+            messengerPackageName: z.string(),
+            query: z.object({
+                sender: z.string(),
+                message: z.string(),
+                isGroup: z.boolean(),
+                groupParticipant: z.string(),
+                ruleId: z.number(),
+                isTestMessage: z.boolean()
+            })
+        }
+    );
+
+    
+    const { query } = appJson.parse(request.body);
+
+    const driver = await prisma.driver.findUnique({
+        where: {
+            phone_number: query.groupParticipant
+        }
+    })
+
+    driver.online = query.message.toUpperCase().trim() == "ONLINE";
+    
+    let messageToReturn = `O motorista ${driver.name} está ${driver.online ? "online 🟢" : "offline 🔴"}!`;
+    
+    return reply
+        .code(200)
+        .header("Content-type", "application/json;charset=utf-8")
+        .send({ "replies": 
+                {
+                    "message": messageToReturn
+                } 
+              }
+            );
+})
+
+app.post('/message', async (request, reply) => {
+
+    const driversOn = await prisma.driver.findMany({
+        where: {
+            online: true
+        }
+    });
+
+    shuffle(driversOn);
+    let messageToReturn = "Olá, tudo bem? Espero que sim!\nEstou indisponível no momento! 😓";
+    if (driversOn.length > 0)
+        messageToReturn += "\nMas, a RGS conta com motoristas preparados para lhe atender! 🚗";
+    for (let i = 0; i < driversOn.length; i++) {
+        let emoji = "🔷";
+        if (!driversOn[i].man)
+            emoji = "🟣";
+        messageToReturn += `${emoji} ${driversOn[i].name}: ${driversOn[i].phone_number}`
+    }
+
+    return reply
+        .code(200)
+        .header("Content-type", "application/json;charset=utf-8")
+        .send({ "replies": 
+                {
+                    "message": messageToReturn
+                } 
+              }
+            );
 })
 
 app.listen({
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333
 }).then(() => console.log("HTTP Server is running"))
+
+
+function shuffle(array: any) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+}
