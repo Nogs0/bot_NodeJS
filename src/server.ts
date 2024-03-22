bee02fc599141b06a6e3e3adbdd8bda1f7bf3f8f
João Nogueira <108362664+Nogs0@users.noreply.github.com>
2024-03-22 09:11:45 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ app.post('/message', async (request, reply) => {
         });
 
         driversOn = shuffle(driversOn);
-        messageToReturn = "Olá, tudo bem? Espero que sim!\nEstou indisponível no momento! 😓\nSe for agendamento, respondo em alguns minutos!😃";
+        messageToReturn = "Olá, tudo bem? Espero que sim!\nEstou indisponível no momento! 😓\nSe for agendamento, respondo em alguns minutos! 😃";
         if (driversOn.length > 0)
             messageToReturn += "\nMas, a FNC conta com motoristas preparados para lhe atender! 🚗";
         for (let i = 0; i < driversOn.length; i++)
